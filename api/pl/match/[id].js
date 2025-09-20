25ab7c1c756b7730c87bc60d6a83e746b23e4957
gaiore99 <gaiore@hotmail.com>
2025-09-20 16:05:36 +0300

    Update [id].js

--- api/pl/match/[id].js
+++ api/pl/match/[id].js
@@ -1,3 +1,10 @@
+headers:{
+  "Origin":"https://www.premierleague.com",
+  "Referer":"https://www.premierleague.com/",
+  "Accept":"application/json",
+  "Accept-Language":"en-US,en;q=0.9",
+  "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) FPLCafe/3.0"
+},
 export const config = { runtime: 'edge' };
 export default async function handler(req){
   const url=new URL(req.url); const id=url.pathname.split('/').pop();
